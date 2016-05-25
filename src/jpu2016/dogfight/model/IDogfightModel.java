263c96325b0be4e2110829224f363acccbeead58
Vasily <Vasily@10-255-30-56>
2016-05-25 14:55:01 +0200

    dogfightmodel IIIII

--- src/jpu2016/dogfight/model/IDogfightModel.java
+++ src/jpu2016/dogfight/model/IDogfightModel.java
@@ -1,6 +1,13 @@
 package jpu2016.dogfight.model;
 
+import java.util.ArrayList;
+
 public interface IDogfightModel 
 {
-
+	public IArea getArea();
+	public void buildArea(Dimension dimension);
+	public void addMobile(IMobile Mobile);
+	public ArrayList<IMobile> getMobiles();
+	public IMobile getMobileByPlayer(int player);
+	public void setMibilesHavesMoved();
 }
